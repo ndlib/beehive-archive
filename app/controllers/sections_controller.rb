5dc17a333c092086135bdea27b21204cf261f01e
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-11-14 16:17:01 -0500

    add section routes

---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,43 @@
+class SectionsController < ApplicationController
+
+  def index
+    @sections = Section.all
+  end
+
+
+  def show
+
+  end
+
+
+  def create
+    @section = Section.new(section_params)
+
+    respond_to do |format|
+      if @section.save
+        format.html { redirect_to @section, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @section }
+      else
+        format.html { render :new }
+        format.json { render json: @section.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def update
+
+  end
+
+
+  def destroy
+
+  end
+
+
+  protected
+
+    def section_params
+      params.require(:section).permit(:title, :image, :item_id, :description)
+    end
+end
