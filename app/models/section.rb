6047b2a63df2ea5275fb5fcfb6f03e31642df86d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-01-21 14:32:56 -0500

    changed the validation to force the type inclusion

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,6 @@ class Section < ActiveRecord::Base
   belongs_to :showcase
 
   validates :image, :item_id, :showcase, :display_type, presence: true
+
+  validates :display_type,  inclusion: { in: %w(image text) }
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -9,10 +9,25 @@ RSpec.describe Section do
     end
   end
 
-  [:item_id, :image, :showcase, :display_type].each do | field |
+  [:item_id, :image, :showcase].each do | field |
     it "requires the field, #{field}" do
       expect(subject).to have(1).error_on(field)
     end
+  end
+
+  it "validates display_type to allow image " do
+    subject.display_type = 'image'
+    expect(subject).to have(0).errors_on(:display_type)
+  end
+
+  it "validates display_type to allow text " do
+    subject.display_type = 'text'
+    expect(subject).to have(0).errors_on(:display_type)
+  end
+
+  it "error display_type on other than text/image " do
+    subject.display_type = '2324324'
+    expect(subject).to have(1).errors_on(:display_type)
   end
 
 end
