59fa787996ae1691f8f6cabbf5cf0590dd9c2d39
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-01-23 13:26:11 -0500

    remove display type from section

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,6 @@
 class Section < ActiveRecord::Base
   belongs_to :showcase
 
-  validates :image, :item_id, :showcase, :display_type, presence: true
+  validates :showcase, presence: true
 
-  validates :display_type,  inclusion: { in: %w(image text) }
 end
--- db/migrate/20150121190739_add_type_to_section.rb
+++
@@ -1,6 +0,0 @@
-class AddTypeToSection < ActiveRecord::Migration
-  def change
-    add_column :sections, :display_type, :string
-    add_index :sections, :display_type
-  end
-end
