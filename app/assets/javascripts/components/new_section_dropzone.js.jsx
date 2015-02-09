9df21ca04a26ed74e928823ca4de1b4f2a62c849
Dan Wolfe <dwolfe2@nd.edu>
2015-02-09 14:56:16 -0500

    cleanup css

--- app/assets/javascripts/components/new_section_dropzone.js.jsx
+++ app/assets/javascripts/components/new_section_dropzone.js.jsx
@@ -33,9 +33,9 @@ var NewSectionDropzone = React.createClass({
       <div className={this.classes()} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} onMouseUp={this.onDrop}>
         <div className="section-spacer-inner">
           <div className="section-spacer-content">
-            <h3>Add New Section</h3>
+            <h4>Add New Section</h4>
             <p>
-              Drag a saved item or
+              Drag a saved item or&nbsp;
               <a href={url}><strong>Add Text</strong></a>
             </p>
           </div>
