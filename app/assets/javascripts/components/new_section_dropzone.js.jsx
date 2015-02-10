14a768c6edabf13611d10a729abab37d3fc28f1f
Dan Wolfe <dwolfe2@nd.edu>
2015-02-10 13:33:26 -0500

    CSS Cleanup on Showcase Edit page
    * grab and grabbing
    * drop target styling
    * Replace drag content with image or text title and very tiny dummy text.

--- app/assets/javascripts/components/new_section_dropzone.js.jsx
+++ app/assets/javascripts/components/new_section_dropzone.js.jsx
@@ -32,12 +32,10 @@ var NewSectionDropzone = React.createClass({
     return (
       <div className={this.classes()} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} onMouseUp={this.onDrop}>
         <div className="section-spacer-inner">
-          <div className="section-spacer-content">
+          <div className="section-spacer-content center">
             <h4>Add New Section</h4>
-            <p>
-              Drag a saved item or&nbsp;
-              <a href={url}><strong>Add Text</strong></a>
-            </p>
+            <p><i className="glyphicon glyphicon-picture super-large-icon"></i></p>
+            <p>Drag a saved item or<br/><a href={url}><strong>Add Text</strong></a></p>
           </div>
         </div>
      </div>);
--- app/assets/javascripts/components/section.js.jsx
+++ app/assets/javascripts/components/section.js.jsx
@@ -98,10 +98,17 @@ var Section = React.createClass({
     } else {
       dragclass = "" + dragclass + " hidden";
     }
+    dragContent = "";
+    if (this.props.section.image) {
+      dragContent = (<img src={this.props.section.image} className="small-image-dragging" />);
+    } else {
+      gibberishTextString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.";
 
+      dragContent = (<div className="small-text-dragging"><h4>{this.props.section.title}</h4><p>{gibberishTextString}</p></div>);
+    }
     return (
       <div className="section" onMouseDown={this.onMouseDown} >
-        <div className={dragclass} style={this.style()}> DRAG ME !!</div>
+        <div className={dragclass} style={this.style()}>{dragContent}</div>
         <SectionImage section={this.props.section} />
         <SectionDescription section={this.props.section} />
         <div className="section-edit" onClick={this.handleClick}>Edit</div>
